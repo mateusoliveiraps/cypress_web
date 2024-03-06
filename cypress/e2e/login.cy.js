bf85fb07773bb1524d1111b2d859c89bba928cea
= <=>
2024-03-06 18:48:46 -0300

    Ajustes de aplicação de acesso

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,26 +1,21 @@
 /// <reference types="cypress" />
 
 describe('Login', () => {
-    // beforeEach(() => {
-    //     cy.visit('/login')
-    // })
+    beforeEach('Acesso a aplicação', () => {
+        cy.visit('/login')
+    })
 
     it('Login com Sucesso', () => {
-        cy.visit('https://automationpratice.com.br/login')
-
-        cy.get('#user').type('eduardo.finotti@qazando.com.br')
+        cy.get('#user').type('mateus.oliveira@qazando.com.br')
         cy.get('#password').type('123456')
 
         cy.get('#btnLogin').click()
 
         cy.get('#swal2-title').should('have.text', 'Login realizado')
-
     })
 
     it('Senha inválida', () => {
-        cy.visit('https://automationpratice.com.br/login')
-
-        cy.get('#user').type('eduardo.finotti@qazando.com.br')
+        cy.get('#user').type('mateus.oliveira@qazando.com.br')
         cy.get('#password').type('123')
 
         cy.get('#btnLogin').click()
@@ -30,8 +25,8 @@ describe('Login', () => {
     })
 
     it('E-mail inválido', () => {
-        cy.visit('https://automationpratice.com.br/login')
 
+        cy.get('#user').type('mateus')
         cy.get('#password').type('123456')
 
         cy.get('#btnLogin').click()
@@ -40,16 +35,14 @@ describe('Login', () => {
     })
 
     it('Senha não informada', () => {
-        cy.visit('https://automationpratice.com.br/login')
 
-        cy.get('#user').type('eduardo.finotti@qazando.com.br')
+        cy.get('#user').type('mateus.oliveira@qazando.com.br')
         cy.get('#btnLogin').click()
 
         cy.get('.invalid_input').should('have.text', 'Senha inválida.')
     })
 
     it('E-mail não informado', () => {
-        cy.visit('https://automationpratice.com.br/login')
 
         cy.get('#password').type('123456')
         cy.get('#btnLogin').click()
@@ -58,8 +51,7 @@ describe('Login', () => {
     })
 
     it('Campos não informado', () => {
-        cy.visit('https://automationpratice.com.br/login')
-
+    
         cy.get('#btnLogin').click()
 
         cy.get('.invalid_input').should('have.text', 'E-mail inválido.')
